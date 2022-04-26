f6f90be74173ddd522cbe73f76d2186fcadf5c82
Rafael Muniz <58229800+RafaelMunizz@users.noreply.github.com>
2022-04-26 00:38:22 -0300

    Update FXMLInitialController.java

--- JavaBasico/src/control/FXMLInitialController.java
+++ JavaBasico/src/control/FXMLInitialController.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package control;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -14,7 +19,21 @@ import javafx.fxml.Initializable;
  * @author rafae
  */
 public class FXMLInitialController implements Initializable {
-
+    
+    //SceneController
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    @FXML
+    public void switchToGame(ActionEvent event) throws IOException {
+        
+        root = FXMLLoader.load(getClass().getResource("/view/FXMLGame.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     /**
      * Initializes the controller class.
      */
