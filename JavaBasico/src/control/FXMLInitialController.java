0c776ac428ce4efaebf4a1cc45ab65e738d16dcb
Rafael Muniz <58229800+RafaelMunizz@users.noreply.github.com>
2022-04-27 00:07:36 -0300

    Update FXMLInitialController.java

--- JavaBasico/src/control/FXMLInitialController.java
+++ JavaBasico/src/control/FXMLInitialController.java
@@ -4,14 +4,18 @@ package control;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * FXML Controller class
@@ -26,14 +30,23 @@ public class FXMLInitialController implements Initializable {
     private Parent root;
     
     @FXML
-    public void switchToGame(ActionEvent event) throws IOException {
+    public void handleButtonAction_mudarParaJogo(ActionEvent event) throws IOException {
         
         root = FXMLLoader.load(getClass().getResource("/view/FXMLGame.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
+    
+    @FXML
+    public void handleButtonAction_FecharJogo(ActionEvent event) throws IOException {
+        
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    
     /**
      * Initializes the controller class.
      */
