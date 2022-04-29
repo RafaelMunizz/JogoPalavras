6850d9feca01ceb5244c9231e47d461bc9d5578f
Rafael Muniz <58229800+RafaelMunizz@users.noreply.github.com>
2022-04-29 01:40:39 -0300

    Create FXMLCreditsController.java

---
+++ JavaBasico/src/control/FXMLCreditsController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author rafae
+ */
+public class FXMLCreditsController implements Initializable {
+
+    @FXML
+    private Button btn_MudarPraMenu1;
+
+    @FXML
+    void handleButtonAction_VoltarParaMenu(ActionEvent event) {
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
