e4e79b44136b6cd2d3a7f706660cc890d6348690
Rafael Muniz <58229800+RafaelMunizz@users.noreply.github.com>
2022-04-30 00:05:17 -0300

    Update FXMLInitialController.java

--- JavaBasico/src/control/FXMLInitialController.java
+++ JavaBasico/src/control/FXMLInitialController.java
@@ -4,18 +4,14 @@ package control;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 /**
  * FXML Controller class
@@ -23,41 +19,47 @@ import javafx.stage.WindowEvent;
  * @author rafae
  */
 public class FXMLInitialController implements Initializable {
-    
-    //SceneController
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-    
+            
     @FXML
     public void handleButtonAction_mudarParaJogo(ActionEvent event) throws IOException {
-        
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLGame.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
     
+        Stage stageJogo;
+        Scene sceneJogo;
+        Parent rootJogo;
+    
+        rootJogo = FXMLLoader.load(getClass().getResource("/view/FXMLGame.fxml"));
+        stageJogo = (Stage)((Node)event.getSource()).getScene().getWindow();
+        sceneJogo = new Scene(rootJogo);
+        stageJogo.setScene(sceneJogo);
+        stageJogo.setResizable(false);
+        stageJogo.show();
+    
+    }
     @FXML
     public void handleButtonAction_FecharJogo(ActionEvent event) throws IOException {
         
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.close();
+        Stage stageFechar;
+        stageFechar = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stageFechar.close();
     }
     
-        @FXML
-    void handleButtonAction_mudarParaCreditos(ActionEvent event) {
-
+    @FXML
+    public void handleButtonAction_mudarParaCreditos(ActionEvent event) throws IOException {
+       
+        Stage stageCreditos;
+        Scene sceneCreditos;
+        Parent rootCreditos;
+        
+        rootCreditos = FXMLLoader.load(getClass().getResource("/view/FXMLCredits.fxml"));
+        stageCreditos = (Stage)((Node)event.getSource()).getScene().getWindow();
+        sceneCreditos = new Scene(rootCreditos);
+        stageCreditos.setScene(sceneCreditos);
+        stageCreditos.setResizable(false);
+        stageCreditos.show();
     }
     
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
 }
