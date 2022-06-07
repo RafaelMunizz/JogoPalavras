52673ade5038984971789c43e6af573370472797
Rafael Muniz <58229800+RafaelMunizz@users.noreply.github.com>
2022-06-06 21:55:55 -0300

    Create ConnectionSQLite.java

---
+++ JavaBasico/src/Connection/ConnectionSQLite.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rafae
+ */
+public class ConnectionSQLite {
+  
+    private Connection conexao;
+
+    /**
+     * Conecta a um banco de dados (cria o banco se ele não existir)
+     *
+     * @return
+     */
+    public boolean conectar() {
+
+        try {
+
+            String url = "jdbc:sqlite:banco_de_dados/banco_sqlite.db";
+
+            this.conexao = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+
+        System.out.println("conectou!!!");
+        
+        return true;
+    }
+
+    public boolean desconectar() {
+
+        try {
+            if (this.conexao.isClosed() == false) {
+                this.conexao.close();
+            }
+
+        } catch (SQLException e) {
+
+            System.err.println(e.getMessage());
+            return false;
+        }
+        System.out.println("desconectou!!!");
+        return true;
+
+    }
+}
